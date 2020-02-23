b4adf745026033375c3651fa484df2752085f982
Abdulkereem <abdulkereemokereem@gmail.com>
2020-02-23 19:42:13 +0000

    progress on the dev

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -5,8 +5,14 @@ void main() => runApp(MaterialApp(
 ));
 
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
 
+  @override
+  _NinjaCardState createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+  int ninjaLevel = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +22,15 @@ class NinjaCard extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:() {
+          setState(() {
+             ninjaLevel +=1;
+          });
+        } ,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800] ,
       ),
 
       body: Padding(
@@ -60,7 +75,7 @@ class NinjaCard extends StatelessWidget {
             ),
             SizedBox(height: 10.0,),
             Text(
-              '8',
+              '$ninjaLevel',
               style: TextStyle(
                 color: Colors.amberAccent[200],
                 letterSpacing: 2.0,
